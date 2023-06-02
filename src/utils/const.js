25523c3c9a442882d34d0beae61705a0af639d7f
semico <yubo.hu@gmail.com>
2023-06-02 10:39:27 +0800

    Update const.js

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoiaHV5dWJvIiwiYSI6ImNsaWR5ZWw5djB3bWQzZW1ua29odTJuOHcifQ.0VsfJ7AtYmC1Y8poeWDfng';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
